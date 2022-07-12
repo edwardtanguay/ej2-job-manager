722b39e2128a5d2851dd2709486a7f2b1d1b3882
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-12 15:07:40 +0200

    has cors error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.scss'
+import { useState, useEffect } from 'react';
+import './App.scss';
+import axios from 'axios';
+
+const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 function App() {
-  return (
-    <div className="App">
-      <h1>EJ2 Job Manager</h1>
-      <p>Welcome to this site.</p>
-    </div>
-  )
+    const [jobSources, setJobSources] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            setJobSources((await axios.get(backend_url)).data);
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>EJ2 Job Manager</h1>
+        <p>There are {jobSources.length} job sources:</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
